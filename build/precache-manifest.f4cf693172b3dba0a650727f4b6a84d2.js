fb25353f40a8bbbb9da82faa342d8f0740309270
Archi <jaiswalarchi07@gmail.com>
2021-09-24 12:53:43 +0530

    Added all folders

---
+++ build/precache-manifest.f4cf693172b3dba0a650727f4b6a84d2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219d826544844cf17223f05f6e974851",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c5ea549f9d8b5707255",
+    "url": "/static/js/2.45e18ea7.chunk.js"
+  },
+  {
+    "revision": "2a37d40449ce3c295946",
+    "url": "/static/js/main.dfbb896c.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PagesIcon from '@material-ui/icons/Pages';
+// import WbIncandescentIcon from '@material-ui/icons/WbIncandescent';
+// import EventNoteIcon from '@material-ui/icons/EventNote';
+// import NoteIcon from '@material-ui/icons/Note';
+// import BlurOnIcon from '@material-ui/icons/BlurOn';
+function Header() {
+  return (
+    <header>
+      <h1>
+      <PagesIcon />
+      &#160;Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
